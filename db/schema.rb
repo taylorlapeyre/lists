bc237f8fc41aa4b2a690b3296b689c1a89452c34
Taylor Lapeyre <taylorlapeyre@gmail.com>
2015-03-14 12:12:49 -0500

    add position column to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313234647) do
+ActiveRecord::Schema.define(version: 20150314171132) do
 
   create_table "items", force: :cascade do |t|
     t.integer "parent_id"
     t.string  "title"
     t.text    "content"
     t.integer "value"
+    t.integer "position"
   end
 
 end
